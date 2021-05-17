f32ba83769f4d341ee91642d447d044c95b66290
Arnon <arnon120@gmail.com>
2021-05-17 12:16:01 +0300

    Notes

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -15,6 +15,20 @@ class Solution:
         sum_index = (n**2 + n)//2
         return sum_index-sum_list
 
+"""
+Note the #1 solution is:
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length = len(nums)
+        total = length * (length + 1) // 2
+        return total - sum(nums)
+
+    which is exactly the same code but uses better functions: 
+    sum_index = total, (1 line <-> 1 line, but better implement)
+    sum_list = sum(nums) (3 line <-> 0 line, using python inner code)
+
+"""
+
 nums = [9,2,4,8,3,5,7,0,1]
 out = Solution().missingNumber2(nums)
 print(out)

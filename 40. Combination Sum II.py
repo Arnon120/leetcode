32eb9e734ae006bb149448a0086adc298e4081f2
Arnon <arnon120@gmail.com>
2021-05-17 10:59:59 +0300

    89% time

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -1,51 +1,24 @@
-import heapq
+from collections import Counter
+from typing import List
+
 class Solution:
-    def combinationSum2(self, candidates, target):
-        candidates.sort()
-        len_cand = len(candidates)
-        out_list = []
-        heapq.heapify(out_list)
-        stack_for_indices = []
-        cur_stack = []
-        cur_sum = 0
-        i = 0
-        while i < len_cand or len(cur_stack) > 0:
-            if i < len_cand:
-                if cur_sum + candidates[i] <= target:
-                    stack_for_indices.append(i)
-                    cur_stack.append(candidates[i])
-                    i += 1
-                    cur_sum += cur_stack[-1]
-                    if cur_sum == target:
-                        # We found a match!
-                        cur_to_insert = cur_stack.copy()
-                        if cur_to_insert not in out_list:
-                            heapq.heappush(out_list,cur_stack.copy())
-                        # clean up structures after a match
-                        cur_top = cur_stack.pop()
-                        stack_for_indices.pop()
-                        cur_sum -= cur_top
-                        i += 1
-                else: # cur_sum + candidates[i] > target:
-                    if len(cur_stack) >0:
-                        cur_top = cur_stack.pop()
-                        i = stack_for_indices.pop() + 1
-                        cur_sum -= cur_top
-                    else:
-                        break
-            else:
-                i = stack_for_indices.pop() + 1
-                cur_top = cur_stack.pop()
-                cur_sum -= cur_top
-        return out_list
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target + 1)]
+        #candidates.sort()
+        candidates_count = Counter(candidates)
+        dp[0].append([])
+        for c,n in candidates_count.most_common():
+            for j in range(1,n+1):
+                appended = [c for _ in range(j)]
+                added = c*j
+                for i in range(added, target + 1):
+                    for combo in dp[i-added]:
+                        if len(combo) == 0 or combo[-1] != c:
+                            dp[i].append(combo + appended)
+        return dp[target]
 
-candidates = [
-    1,1,1,1,1,
-    1,1,1,1,1]
 
-target = 9
+candidates = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+target = 10
 output = Solution().combinationSum2(candidates,target)
 print(output)
-        
-
-

4bd0c58a78df32843680425fd1b7c73d96ed7ba6
Arnon <arnon120@gmail.com>
2021-05-17 12:12:18 +0300

    The_Xor_solution

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -3,17 +3,18 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
+        x = n
         for i in range(n):
-            j = nums[i]
-            while j < n and j != nums[j]:
-                k = nums[j]
-                nums[j] = j
-                j = k
-        for i in range(n):
-            if i != nums[i]:
-                return i
-        return n
+            x^=i^nums[i]
+        return x
+    def missingNumber2(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum_list = 0
+        for c in nums:
+            sum_list += c
+        sum_index = (n**2 + n)//2
+        return sum_index-sum_list
 
-nums = [3,0,1]
-out = Solution().missingNumber(nums)
+nums = [9,2,4,8,3,5,7,0,1]
+out = Solution().missingNumber2(nums)
 print(out)

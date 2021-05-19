f207f5d1af83556e205817f7673fe67e97147e71
Arnon <arnon120@gmail.com>
2021-05-19 11:30:58 +0300

    Before Restart

--- 37. Sudoku Solver.py
+++ 37. Sudoku Solver.py
@@ -1,4 +1,5 @@
 import itertools
+import copy
 class Solution:
     def isValidSudoku(self, board) -> bool:
         rec_set = set()
@@ -18,161 +19,70 @@ class Solution:
     
     #def solveSudoku(self, board: List[List[str]]) -> None:
     def solveSudoku(self, board) -> None:
-        rec_set = set()
-        # init rec_set
-        for i in range(0,9):
-            for j in range(0,9):
-                if board[i][j]!='.':
-                    cur = board[i][j]
-                    #if (i,cur) in rec_set or (cur,j) in rec_set or (i//3,j//3,cur) in rec_set:
-                    #    return False
-                    rec_set.add((i,cur))
-                    rec_set.add((cur,j))
-                    rec_set.add((i//3,j//3,cur))
-        queue = list()
-        for i in range(0,9):
-            for j in range(0,9):
-                if board[i][j] == '.':
-                    board[i][j] = '123456789'
-                    for cur in '123456789':
-                        if (i,cur) in rec_set or (cur,j) in rec_set or (i//3, j//3, cur) in rec_set:
-                            board[i][j] = board[i][j].replace(cur,'')
-                    if len(board[i][j]) == 1:
-                        cur = board[i][j]
-                        queue.append((i,j))
-                        # seems like we don't use this...
-                        rec_set.add((i,cur))
-                        rec_set.add((cur,j))
-                        rec_set.add((i//3,j//3,cur))
-        def look_for_missing_num(unit_type: str, k: int) -> bool:
-            d = dict({})
-            was_resolt = False
+        def preform_obvious_moves(board):
+            # init queue, dicts and board
+            queue = []
+            initialized_dict = dict(zip([str(i) for i in range(9)],[[i for i in range(9)] for i in range(9)]))
+            # Row, Col, Cell  struct_num in {0,1,2}
+            # k allways corresponds to number in struct 
+            dicts = (copy.deepcopy(initialized_dict),
+                    copy.deepcopy(initialized_dict), 
+                    initialized_dict)
+            def update_dicts(dicts,i,j,val):
+                for struct_type in range(3):
+                    if struct_type == 0:
+                        struct_num = i
+                        k = j
+                    elif struct_type == 1:
+                        struct_num = j
+                        k = i
+                    else: # struct_type == 2
+                        struct_num = 3 * (i // 3) + (j // 3)
+                        k = 3*(i % 3) + (j % 3)
+                    # removes the consideration of the value board[i][j]
+                    dicts[struct_type][struct_num].pop(val , None)
+                    # for any other value it can not appear in location k
+                    for val, lis in dicts[struct_type][struct_num].items():
+                        lis.remove(k)
+                        if len(lis) == 1:
+                            """
+                            Triger something!
+                            """
+                            print('Should trigger something!')
             for i in range(9):
-                if unit_type == 'Row':
-                    val = board[k][i]
-                elif unit_type == 'Col':
-                    val = board[i][k]
-                else: # unit_type == 'Cell':
-                    val = board[3*(k // 3) + (i//3)][3*(k % 3) + (i % 3)]
-                if len(val) > 1:
-                    for cur in val:
-                        if cur not in d.keys():
-                            d[cur] = [i]
-                        else:
-                            d[cur].append(i)
-            for key, value in d.items():
-                if len(value) == 1:
-                    was_resolt = True
-                    if unit_type == 'Row':
-                        row_num = k
-                        col_num = value[0]
-                    elif unit_type == 'Col':
-                        row_num = value[0]
-                        col_num = k
-                    else: # unit_type == 'Cell':
-                        row_num = 3*(k // 3) + value[0] // 3
-                        col_num = 3*(k % 3) + value[0] % 3
-                    board[row_num][col_num] = key
-                    queue.append((row_num,col_num))
-                    rec_set.add((row_num,key))
-                    rec_set.add((key,col_num))
-                    rec_set.add((row_num//3,col_num//3,key))
-            return was_resolt
-
-        while len(rec_set) < 3 * 9 * 9 - 1:
-            while len(queue) > 0 and len(rec_set) < 3 * 9 * 9 - 1:
-                tup = queue.pop()
+                for j in range(9):
+                    if len(board[i][j]) > 1:
+                        continue
+                    elif board[i][j] == '.':
+                        board[i][j] = '123456789'
+                    else:
+                        queue.append((i,j,board[i][j]))
+                        update_dicts(dicts,i,j,board[i][j])
+                        # problem, we have yet to remove the elements form board.
+            while len(queue) > 0:
+                tup = queue.pop(0)
                 i = tup[0]
                 j = tup[1]
-                cur = board[i][j]
-                for k in range(0,9):
-                    vals = board[i][k]
-                    if len(vals) != 1:
-                        vals = vals.replace(cur,'')
-                        board[i][k] = vals
-                        #if not self.isValidSudoku(board):
-                        #    print(tup)
-                        if len(vals) == 1:
-                            queue.append((i,k))
-                            rec_set.add((i,vals))
-                            rec_set.add((vals,k))
-                            rec_set.add((i//3,k//3,vals))
-                    vals = board[k][j]
-                    if len(vals) != 1:
-                        vals = vals.replace(cur,'')
-                        board[k][j] = vals
-                        #if not self.isValidSudoku(board):
-                        #    print(tup)
-                        if len(vals) == 1:
-                            queue.append((k,j))
-                            rec_set.add((k,vals))
-                            rec_set.add((vals,j))
-                            rec_set.add((k//3,j//3,vals))
-                    vals = board[3*(i//3) + (k // 3) ][ 3*(j//3) + (k % 3)]
-
-                    if len(vals) != 1:
-                        vals = vals.replace(cur,'')
-                        board[3*(i//3) + (k // 3) ][ 3*(j//3) + (k % 3)] = vals
-                        #if not self.isValidSudoku(board):
-                        #    print(tup)
-                        if len(vals) == 1:
-                            queue.append((3*(i//3) + (k // 3), 3*(j//3) + (k % 3)))
-                            rec_set.add((3*(i//3) + (k // 3),vals))
-                            rec_set.add((vals,3*(j//3) + (k % 3)))
-                            rec_set.add((i,j,vals))
-            #for i in range(9):
-            #    for k in range(1,10):
-            #        if not (i,str(k)) in rec_set:
-            #            print(i,str(k))
-            #        if not (i,str(k)) in rec_set:
-            #            print(str(k),i)
-            #        if not (i,str(k)) in rec_set:
-            #            print(i // 3, i % 3, str(k))
-            if len(rec_set) < 3 * 9 * 9 - 1:
-                for tup in itertools.product(range(9),['Row','Col','Cell']):
-                    k = tup[0]
-                    unit_type = tup[1]
-                    if look_for_missing_num(unit_type, k):
-                        break
-            if len(rec_set) < 3 * 9 * 9 - 1:
-                #for i in range(9):
-                #    for k in range(1,10):
-                #        if not (i,str(k)) in rec_set:
-                #            print(i,str(k))
-                #        if not (i,str(k)) in rec_set:
-                #            print(str(k),i)
-                #        if not (i,str(k)) in rec_set:
-                #            print(i // 3, i % 3, str(k))
-                print( len(rec_set))
-                print('cool stuff: need better algorithm.')
-                for i in range(9):
-                    pri = '|'
-                    for j in range(9):
-                        if len(board[i][j]) == 1:
-                            pri += board[i][j] + '|'
-                        else:
-                            pri += '.' +'|'
-                    print(pri)
-                print('-------------------')
-        
-        
-        
-        for i in range(9):
-            pri = '|'
-            for j in range(9):
-                if len(board[i][j]) == 1:
-                    pri += board[i][j] + '|'
-                else:
-                    pri += '.' +'|'
-            print(pri)
-        for i in range(9):
-            for k in range(1,10):
-                if not (i,str(k)) in rec_set:
-                    print(i,str(k))
-                if not (i,str(k)) in rec_set:
-                    print(str(k),i)
-                if not (i,str(k)) in rec_set:
-                    print(i // 3, i % 3, str(k))
+                val = tup[2]
+                for k in range(9):
+                    for struct in {'Row','Col','Cell'}:
+                        if struct == 'Row':
+                            row_num = i
+                            col_num = k
+                        elif struct == 'Col':
+                            row_num = k
+                            col_num = j
+                        else: # stuct = 'Cell'
+                            row_num = 3 * (i // 3) + (k // 3)
+                            col_num = 3 * (j // 3) + (k % 3)
+                        if len(board[row_num][col_num]) > 1:
+                            board[row_num][col_num].replace(val,'')
+                            new_val = board[row_num][col_num]
+                            if len(new_val) == 1:
+                                queue.append((row_num,col_num,new_val))
+                                update_dicts(dicts,row_num,col_num,new_val,)
+            print (board)
+        preform_obvious_moves(board)
             
         
 
@@ -183,9 +93,5 @@ board_0 = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".
 board_1 = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]]
 board_2 = [[".",".",".","2",".",".",".","6","3"],["3",".",".",".",".","5","4",".","1"],[".",".","1",".",".","3","9","8","."],[".",".",".",".",".",".",".","9","."],[".",".",".","5","3","8",".",".","."],[".","3",".",".",".",".",".",".","."],[".","2","6","3",".",".","5",".","."],["5",".","3","7",".",".",".",".","8"],["4","7",".",".",".","1",".",".","."]]
 boards = [board_0,board_1,board_2]
-Solution().solveSudoku(boards[2])
-"""
-This prompts an error with the calculation.
-"""
-
-print(boards[2])    
+Solution().solveSudoku(boards[0])
+print(boards[0])    
